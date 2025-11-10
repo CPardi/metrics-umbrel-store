2e0ccf78573f753566f52a3ff0cd00a191de8bc4
C P <13357398-ChriPar@users.noreply.gitlab.com>
2025-11-10 12:52:47 +0700

    Use version 4 syntax for yq

--- scripts/update-umbrel-version.sh
+++ scripts/update-umbrel-version.sh
@@ -5,9 +5,9 @@ name="web"
 find . -type f -name 'umbrel-app.yml' | while read -r file; do
   echo "--- Found: $file"
   directory=$(dirname "$file")
-  version=$(yq ".services.$name.image" "$directory/docker-compose.yml" | grep -v null | sed -E 's/.*:([^@]+)@.*/\1/')
+  version=$(yq e ".services.$name.image" "$directory/docker-compose.yml" | grep -v null | sed -E 's/.*:([^@]+)@.*/\1/')
   if [ -n "$version" ]; then
-    yq --in-place --yaml-output --yaml-roundtrip ".version = \"$version\"" "$file"
+    yq e -i ".version = \"$version\"" "$file"
   fi
   echo ""
 done
